ea571822cb804b1d0e0acfeb9211b0b85430c8c3
Kazumi Karbowski <kazkazinus@gmail.com>
2017-12-19 06:07:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,22 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
 
 function init() {
-  // Write your JavaScript code inside the init() function
 
+      var keys = [];
+      var index = 0;   
+      document.body.addEventListener('keydown', function(e){
+
+          keys.push(e.index);
+
+          if (keys.toString().indexOf(code) >= 0) {
+
+               alert("Hurray!");
+
+              keys = [];
+          }
+      }, true);
+  
 }
+
+

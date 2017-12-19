c9a7996e0107704e8c0bb946bb769d3bdc385f1d
Kazumi Karbowski <kazkazinus@gmail.com>
2017-12-19 06:07:42 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,20 +2,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 
 function init() {
-
-      var keys = [];
-      var index = 0;   
-      document.body.addEventListener('keydown', function(e){
-
-          keys.push(e.index);
-
-          if (keys.toString().indexOf(code) >= 0) {
-
-               alert("Hurray!");
-
-              keys = [];
-          }
-      }, true);
+  let index = 0;
+ -} 		 +  document.body.addEventListener('keydown', function(e){
+ +   const key = parseInt(e.detail || e.which)
+ +   if (key === code[index]) {
+ +     index++
+ +     if (index === code.length) {
+ +       alert('Congratulations')
+ +       index = 0
+ +     }
+ +   }
+ +   else {
+ +     index = 0
+ +   }
+ + })
+ +}
   
 }
 
